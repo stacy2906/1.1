562b391583af6c481a51dfd8cc7acad3b91fff78
stacy2906 <124590794+stacy2906@users.noreply.github.com>
2024-02-11 15:27:55 +0200

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp5
+{
+    internal class Student:Person
+    {
+        private DateTime birth;
+
+        private int kurs;
+
+
+
+        public DateTime Birth
+
+        {
+
+            get { return birth; }
+
+
+
+            set { birth = value; }
+
+        }
+
+        public int Kurs
+
+
+
+        {
+
+
+
+            get { return kurs; }
+
+
+
+            set { kurs = value; }
+
+        }
+
+
+
+        public Student() { }
+
+        public Student(string name, DateTime birth, int kurs) : base(name)
+
+        {
+
+            this.birth = birth;
+
+            this.kurs = kurs;
+
+        }
+
+        public override void Print()
+        {
+
+            Console.WriteLine($"Name: {Name}, Birth: {birth.ToShortDateString()}, Kurs: {kurs}");
+
+        }
+
+
+    }
+}
